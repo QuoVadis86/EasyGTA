000ec3fdfb6036072ab3f83e2b17e04ecbf2738e
QuoVadis <iamasirs@gmail.com>
2024-09-25 12:37:16 +0800

    无需初始化内存

--- Main.py
+++ Main.py
@@ -1,10 +1,8 @@
-from Memory import init as memory_init
-from Scripts import init as script_init
+
 from Utils import init as util_init
 from Listener import init as listener_init
 from UI import init as ui_init
-memory_init()
+
 ui_init()
-script_init()
 util_init()
 listener_init()
