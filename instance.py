b9eb1b006ba488060321f6c9d1c68e827726e259
暗黑破坏神 <iamasirs@gmail.com>
2024-12-20 17:22:23 +0800

    feat: 增加运行态instance

---
+++ instance.py
@@ -0,0 +1,22 @@
+from entity.input import Input
+from entity.weapon import Weapon
+from entity.process import GTA
+from entity.listener import Listener
+
+input: Input = None
+process: GTA = None
+weapon: Weapon = None
+listener: Listener = None
+# is_suspend=False
+
+x2_pressed = False
+left_pressed = False
+space_pressed = False
+tab_pressed = False
+
+def init():
+    global input, process, weapon, listener
+    input = Input()
+    process = GTA()
+    weapon = Weapon()
+    listener = Listener()
